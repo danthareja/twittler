988ad81acaf751b18b699a343f56c9429c27bb4c
Dan Thareja <danthareja@gmail.com>
2014-06-18 16:10:08 -0400

    Minor styling changes

--- js/twittler.js
+++ js/twittler.js
@@ -1,35 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function() {
 var initialTime = moment().subtract('s', 1); //create new moment Date of now() minus one second so first tweets show up correctly.
-var $button = $('.new-tweets');
 var tweetIndex = 0; //Keeps track of how many tweets are currently in the list
-
 
 var showTweets = function() {
 	var tweetCount = streams.home.length - 1;  //Pulls new tweet length
-	while(tweetCount >= tweetIndex){
+	while (tweetCount >= tweetIndex) {
 	  var tweet = streams.home[tweetIndex];
 	  var time = initialTime.from(moment(tweet.created_at));
 	  var $tweet = $("<div></div>").addClass("tweet");
 	  var $time = $("<time></time>").addClass("time");
-	  var $user = $("<span></span>").addClass("user");
+	  var $user = $("<a href=\"#\"></a>").addClass("user");
 	  
 	  //Set text for each variable
 	  $time.text(" - " + time);
-	  $user.text("@" + tweet.user + ": ");
-	  $tweet.text(tweet.message);
+	  $user.text("@" + tweet.user);
+	  $tweet.text(": " + tweet.message);
 
 	  //Concatenate tweet
 	  $tweet.prependTo($(".tweets"));
 	  $time.appendTo($tweet);
 	  $user.prependTo($tweet);
 
+	  //Increment tweet counter
 	  tweetIndex++;
 	}
-}
+};
 
 showTweets();
 setInterval(showTweets, 500);
 
-$button.on('click', showTweets);
+$(".user").on("click", function(e) {
+	e.preventDefault();
+	console.log(this.text);
+});
 
 });
